89314214dc02e59cced1b730e6aa20e077a58eb8
HansChen <hans5300609@gmail.com>
2022-12-22 12:08:51 +0800

    choseHelper.php chage style

---
+++ choseHelper.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="zh-hant-tw">
+<head>
+	<meta charset="UTF-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<title>組裝幫手</title>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css" integrity="sha512-NmLkDIU1C/C88wi324HBc+S2kLhi08PN5GDeUVVVC/BVt/9Izdsc9SVeVfA1UZbY3sHUlDSyRXhCzHfr6hmPPw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+	<!-- <link rel="stylesheet" href="css/style.css" /> -->
+	<link rel="stylesheet" href="css/choseHelper.css" />
+</head>
+<body>
+	<div id="choseHelper">
+		<p>組裝幫手</p>
+		<ul>
+			<a href="#"><li>&#8728;中央處理器CPU<p id="choseCpu">chose</p></li></a>
+			<a href="#"><li>&#8728;主機板MB<p id="choseMb">chose</p></li></a>
+			<a href="#"><li>&#8728;記憶體RAM<p id="choseRam">chose</p></li></a>
+			<a href="#"><li>&#8728;顯示卡GPU<p id="choseGpu">chose</p></li></a>
+			<li>&#8728;儲存裝置Storage<ul>
+				<a href="#"><li>固態硬碟SSD<p id="choseSSD">chose</p></li></a>
+				<a href="#"><li>馬達硬碟HDD<p id="choseHDD">chose</p></li></a>
+			</ul></li>
+			<a href="#"><li>&#8728;電源供應器PSU<p id="chosePsu">chose</p></li></a>
+			<a href="#"><li>&#8728;機殼Case<p id="choseCase">chose</p></li></a>
+		</ul>
+	</div>
+	<div id="product">
+		
+	</div>
+</body>
+</html>
